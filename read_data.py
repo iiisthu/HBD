a31daed0ad1865e953d2ca710879dc932c0d8b5a
wgsjack199213 <wgsjack199213@yeah.net>
2014-12-29 13:05:43 +0800

    basic correlation query

--- read_data.py
+++ read_data.py
@@ -1,31 +1,88 @@
-#import sys
+import sys
 from pyspark import SparkContext
-from pyspark.sql import SQLContext, Row
+from pyspark.sql import *
+
+field_cpu_failure = "host cpu_id time_stamp host_sn reason record_id result source"
+
+def construct_schemaRDD(parts, field_names, field_types):
+	fields = field_names.split()
+	length = len(fields) # the length of true values
+	rowRDD = parts.map(lambda p: tuple([p[i+1] for i in range(length)]))
+	#print len(fields), len(field_types), length
+	columns = [StructField(fields[k], field_types[k], True) for k in range(length)]
+	schema = StructType(columns)
+	schemaRDD = sqlContext.applySchema(rowRDD, schema)
+	
+	return schemaRDD
+
+def get_table_server_rt_status(lines):
+	def convert_types(line):
+		parts = line.split(' ')
+		parts[2] = long(parts[2]) if parts[2] != 'None' else None
+		for k in range(4, 11):
+			parts[k] = long(parts[k]) if parts[k] != 'None' else None
+		return parts
+
+	field_names = "host timestamp host_sn cpupower dimmpower in_temp out_temp pch_temp power power_ratio record_id"
+	field_types = [StringType(), LongType(), StringType(), IntegerType(), IntegerType(), IntegerType(), IntegerType(), IntegerType(), IntegerType(), IntegerType(), StringType()]	
+	parts = lines.map(convert_types)	
+	schemaRDD = construct_schemaRDD(parts, field_names, field_types)
+	table = schemaRDD.registerTempTable("server_rt_status")
+	return table
 
 
-def merge_same(a, b):
-	result = []
-	for x in a:
-		result.append[x]
-	for y in b:
-		pass
-	return result
+def get_table_cpu_performance(lines):
+    def convert_types(line):
+		parts = line.split(' ')
+		parts[3] = long(parts[3]) if parts[3] != 'None' else None
+		parts[5] = int(parts[5]) if parts[5] != 'None' else None
+		parts[7] = float(parts[7]) if parts[7] != 'None' else None
+		return parts
+
+    field_names = "host cpu_id timestamp host_sn cur_freq ht norm pstate record_id turbo"
+    field_types = [StringType(), StringType(), LongType(), StringType(), IntegerType(), StringType(), FloatType(), StringType(), StringType(), StringType()]
+    parts = lines.map(convert_types)
+    schemaRDD = construct_schemaRDD(parts, field_names, field_types)
+    table = schemaRDD.registerTempTable("cpu_performance")
+    return table
+
+
+
+
+###=====================================================================================================
 
 if __name__ == "__main__":
-	sc = SparkContext(appName="StoreDataIntoSparkSQL")
+	sc = SparkContext(appName="ReadDataIntoSparkSQL")
 	sqlContext = SQLContext(sc)
 
 	print "Hello!"
 
 	path = "/app/sys/ra/wgs"
 
-	lines = sc.textFile(path+"/output/cpu_basic/miss/part-000[0-9]*")
-	#missing_logs = lines.filter(lambda l: l.startswith("cpu_basic"))
-	#miss_info = lines.map(lambda l: [l])
-	totalLength = lines.countByValue()
+#	lines1 = sc.textFile(path+"/output/cpu_performance/20141201_0100/part-000[0-9]*")
+#	table_cpu_performance = get_table_cpu_performance(lines1)
+#	results = sqlContext.sql("SELECT host,timestamp,cur_freq,turbo FROM cpu_performance WHERE host = '10.86.148.11'")
+#	results.map(lambda p: "host:" + p.host + " timestamp:" + p.timestamp + " cur_freq:" + p.cur_freq + " turbo:" + p.turbo)
+#	for record in results.collect():
+#		print record
 
-	print totalLength
+#	print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", results
 
-	print "!!!!!!!!!!!!!!!!!!!"
+
+	lines2 = sc.textFile(path+"/output/server_rt_status/20141201_0100/part-000[0-9]*")
+	table_server_rt_status = get_table_server_rt_status(lines2)
+	results = sqlContext.sql("SELECT host,timestamp,cpupower,dimmpower FROM server_rt_status WHERE host = '10.86.148.11' AND timestamp/100 >= 14173669 AND timestamp/100 <= 14173675")
+	records = results.map(lambda p: "host:" + p.host + " timestamp:" + str(p.timestamp) + " cpupower:" + str(p.cpupower) + " dimmpower:" + str(p.dimmpower))
+	for record in records.collect():
+		print record
+#	results = sqlContext.sql("SELECT cpu_performance.host,cpu_performance.timestamp,cpu_performance.cpu_id,cpu_performance.turbo,server_rt_status.cpupower FROM cpu_performance,server_rt_status WHERE cpu_performance.host = '10.86.148.11' AND server_rt_status.host = '10.86.148.11' AND server_rt_status.timestamp - cpu_performance.timestamp < 60 AND server_rt_status.timestamp - cpu_performance.timestamp > -60")	
+	#print results.count()
+#	print "+++++++++++++++++++++++++++++++++"
+#	records = results.map(lambda p: "host:" + p.host + " cpu_id:" + p.cpu_id + " timestamp:" + str(p.timestamp) + " turbo:" + p.turbo + " cpupower:" + str(p.cpupower))
+#	for record in records.collect():
+#	    print record	
+	
+
+	print "###############################"
 
 	sc.stop()
--- store_in_sparksql.py
+++ store_in_sparksql.py
@@ -7,7 +7,8 @@ def get_log(log_name, parts, row, columns, start_index = 4):
 	for col in range(start_index, len(parts)-1, 2):
 		row[parts[col]] = parts[col+1]
     #Keys and values are listed in an arbitrary order which is non-random, varies across Python implementations, and depends on the dictionarys history of insertions and deletions
-	log = log_name + " " + parts[1] + " " + parts[2] + " " + parts[3] + " "
+	#log = log_name + " " + parts[1] + " " + parts[2] + " " + parts[3] + " "
+	log = log_name
 	for key in columns:
 		#log += ' ' + key + ' ' + row.get(key, "None") 
 		log += ' ' + row.get(key, "None")
@@ -17,7 +18,7 @@ def get_log(log_name, parts, row, columns, start_index = 4):
 
 # Some columns of the row maybe missing, so complete some parts
 def complete_cpu_basic(line):
-	columns = ["host_sn", "core_n", "freq", "l2_c", "l3_c", "manufacturer", "model", "sn"]
+	columns = ["host", "cpu_id", "time_stamp", "host_sn", "core_n", "freq", "l2_c", "l3_c", "manufacturer", "model", "sn"]
 
 	p = line.replace(":", " ").split()
 	row = {'host': p[1], 'cpu_id': p[2], 'time_stamp': p[3]}
@@ -33,7 +34,7 @@ def complete_cpu_failure(line):
 	return log
 
 def complete_cpu_performance(line):
-	columns = ["host_sn", "cur_freq", "ht", "norm", "pstate", "record_id", "turbo"]
+	columns = ["host", "cpu_id", "time_stamp", "host_sn", "cur_freq", "ht", "norm", "pstate", "record_id", "turbo"]
 
 	p = line.replace(":", " ").split()
 	row = {'host': p[1], 'cpu_id': p[2], 'time_stamp': p[3]}
@@ -184,11 +185,11 @@ def complete_server_overall_status(line):
 	return log
 
 def complete_server_rt_status(line):
-	columns = ["host_sn", "cpupower", "dimmpower", "in_temp", "out_temp", "pch_temp", "power", "power_ratio"]
+	columns = ["host", "time_stamp", "host_sn", "cpupower", "dimmpower", "in_temp", "out_temp", "pch_temp", "power", "power_ratio", "record_id"]
 
 	p = line.replace(":", " ").split()
-	row = {'host': p[1], 'time_stamp': p[3]}
-	log = get_log("server_rt_status", p, row, columns, 4)
+	row = {'host': p[1], 'time_stamp': p[2]}
+	log = get_log("server_rt_status", p, row, columns, 3)
 	return log
 
 def complete_vd_failure(line):
@@ -199,7 +200,7 @@ def complete_vd_failure(line):
 	log = get_log("vd_failure", p, row, columns, 4)
 	return log
 
-def complte_vd_status(line):
+def complete_vd_status(line):
 	columns = ["host_sn", "label", "level", "pd_n", "pid", "rcp", "record_id", "rw_r"]
 
 	p = line.replace(":", " ").split()
@@ -221,12 +222,14 @@ if __name__ == "__main__":
 
 	lines = sc.textFile(path+"/20141201_0100")
 	#cpu_basic_logs = lines.filter(lambda l: l.startswith("cpu_basic"))
-	cpu_failure_logs = lines.filter(lambda l: l.startswith("cpu_failure"))
+	#cpu_failure_logs = lines.filter(lambda l: l.startswith("cpu_failure"))
+	server_rt_logs = lines.filter(lambda l: l.startswith("cpu_performance"))
 
 	# host cpu_id value(timestamp) core_n freq l2_c l3_c manufacturer sn
 	#cpu_basic_text = cpu_basic_logs.map(complete_cpu_basic)
-	cpu_basic_text = cpu_failure_logs.map(complete_cpu_failure)
-
+	#cpu_basic_text = cpu_failure_logs.map(complete_cpu_failure)
+	
+	server_rt_text = server_rt_logs.map(complete_cpu_performance) 
 
 	#schema_cpu_basic = sqlContext.inferSchema(logs)
 	#schema_cpu_basic.registerTempTable("logs")
@@ -236,9 +239,9 @@ if __name__ == "__main__":
 	#for x in result.collect():
 	#   print x
 
-	cpu_basic_text.saveAsTextFile(path + "/output/cpu_failure/20141201_0100")
-
-	print "!!!!!!!!!!!!!!!!!!!"
+	#server_rt_text.saveAsTextFile(path + "/output/server_rt_status/20141201_0100")	
+	server_rt_text.saveAsTextFile(path + "/output/cpu_performance/20141201_0100")
+	print "!!!!!!!!!!!!!!!!!!!ending"
 
 	sc.stop()
 
